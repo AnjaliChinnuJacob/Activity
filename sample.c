88c5b9560561f2fc340097ad59fb7b21505a3d91
AnjaliChinnuJacob <59722493+AnjaliChinnuJacob@users.noreply.github.com>
2020-01-10 11:56:10 +0530

    Create sample.c

---
+++ sample.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void swap(int*,int*);
+int main() {
+  int a,b;
+  a=10;
+  b=20;
+  swap(&a,&b);
+  printf("a=%d,b=%d\n",a,b);
+  return 0;
+}
+void swap(int *p1,int *p2) {
+  int t=*p1;
+  *p1=*p2;
+  *p2=t;
+}
